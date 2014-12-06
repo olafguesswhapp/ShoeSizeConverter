c7fcac925fdb3e9ae2e82c7ebeea3a9f534a610e
Olaf Peters <olaf@guesswhapp.de>
2014-12-06 12:12:26 +0100

    Initial Commit

---
+++ ShoeSizeConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShoeSizeConverter
+//
+//  Created by Olaf Peters on 06.12.14.
+//  Copyright (c) 2014 Olaf Peters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
