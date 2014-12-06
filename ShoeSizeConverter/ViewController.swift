54e5ae582b7a8ea60be0e85038c1a767fbe68696
Olaf Peters <olaf@guesswhapp.de>
2014-12-06 15:31:06 +0100

    bitfountain Swift lesson 1st Version

--- ShoeSizeConverter/ViewController.swift
+++ ShoeSizeConverter/ViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var WomansLabel: UILabel!
+    @IBOutlet weak var WomanEULabel: UILabel!
+    @IBOutlet weak var MensLabel: UILabel!
+    @IBOutlet weak var MenEULabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +26,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func ConvertButtonPressed(sender: UIButton) {
+        var womanCalculated = Double((inputTextField.text as NSString).doubleValue)
+        WomanEULabel.text = "\(womanCalculated + 30.5)"
+        
+        var menCalculated = inputTextField.text.toInt()!
+        MenEULabel.text = "\(menCalculated + 30)"
+        
+        WomansLabel.hidden = false
+        WomanEULabel.hidden = false
+        MensLabel.hidden = false
+        MenEULabel.hidden = false
+    }
+    
 
 
 }
